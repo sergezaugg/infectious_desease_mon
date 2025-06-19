7822c04b1b9a272423880682795fdd14ca13785b
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-19 14:39:40 +0200

    good to go

--- page00.py
+++ page00.py
@@ -6,22 +6,21 @@
 import streamlit as st
 
 col_aa, col_bb, = st.columns([0.50, 0.20])
-
 with col_aa: 
-
-    with st.container(border=True):
-        st.title("Swiss Influenza Monitoring Visualization") 
-
     with st.container(border=True, ):
-        st.markdown(
-        '''    
-        **SUMMARY**
 
-        A simple dashboard to visualize Influenza monitoring data provided by the Data API of the Swiss Federal Office of Public Health (FOPH).
-
-        For detailed background information and context please see the links in sidebar on the left.
-        
+        st.markdown('''
+        ## CREDITS
+        * ### Data updated weekly by the [Data API](https://www.idd.bag.admin.ch/portal-data) of the Swiss Federal Office of Public Health ([FOPH](https://www.bag.admin.ch/)).
+        * ### Only incidence rate of Infuenza (flu) is visualized.
         ''')
+
+        st.markdown('''### LINKS ''')
+        st.page_link("https://www.bag.admin.ch/", label="Data provided by FOPH")
+        st.page_link("https://www.idd.bag.admin.ch/portal-data", label="Data API")
+        st.page_link("https://www.idd.bag.admin.ch/dataexplorer", label="Official frontend of FOPH")
+        st.page_link("https://www.idd.bag.admin.ch/survey-systems/oblig", label="Mandatory reporting (oblig)")
+        st.page_link("https://www.idd.bag.admin.ch/survey-systems/sentinella", label="Voluntary surveillance (sentinella)")
 
    
     

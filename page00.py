3947fe73d6352e810346b636cd2304bf8dd73ed7
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-20 12:07:09 +0200

    changed sel mode for plot type to select slider

--- page00.py
+++ page00.py
@@ -28,13 +28,17 @@ else:
                 format = "YYYY-MM-DD", label_visibility = "visible",key = "k_date_range", on_change=update_ss, args=["k_date_range", "date_range"])      
     with ca2:
         with st.container(height=120, border=True):
-            ca1, ca2, ca3 = st.columns([0.3, 0.60, 0.25])
+            ca1, ca2, ca3, _ = st.columns([0.3, 0.60, 0.15, 0.15])
             _ = ca1.segmented_control("Data source", options = ['oblig', 'sentinella'], selection_mode="multi",  default = ss["upar"]["selecte_data_sources"],
                                                 key = "k_data_sou", on_change=update_ss, args=["k_data_sou", "selecte_data_sources"]) 
             _ = ca2.segmented_control("Data grouping", options = ['All', 'Age', 'Type', 'Sex', 'Region',], selection_mode="multi", default = ss["upar"]["selecte_data_groupings"],
-                                                key = "k_data_gr", on_change=update_ss, args=["k_data_gr", "selecte_data_groupings"])  
-            _ = ca3.segmented_control("Plot type", options = ['Line', 'Area'], selection_mode="single", default = ss["upar"]["plot_type"], 
-                                                key = "k_plot_type", on_change=update_ss, args=["k_plot_type", "plot_type"])
+                                                key = "k_data_gr", on_change=update_ss, args=["k_data_gr", "selecte_data_groupings"])
+              
+            # _ = ca3.segmented_control("Plot type", options = ['Line', 'Area'], selection_mode="single", default = ss["upar"]["plot_type"], 
+            #                                     key = "k_plot_type", on_change = update_ss, args=["k_plot_type", "plot_type"])
+            _ = ca3.select_slider("Plot type", options = ['Line', 'Area'], value=ss["upar"]["plot_type"], 
+                                                key="k_plot_type", on_change = update_ss, args=["k_plot_type", "plot_type"])
+
             cb1, cb2, cb3 = st.columns([0.3, 0.60, 0.25])
                   
     with st.expander( "Info on incidence metrics"):
